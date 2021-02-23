78a5b34cbb1a07553928525b09d27d7f59a19949
Livia Andai <43793971+a-livia@users.noreply.github.com>
2021-02-23 12:12:58 +0100

    Add files via upload

---
+++ cimletezo-main/cimletezo_ize/src/main/java/Main.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import main.java.Cimletezo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cimletezo cimletezo = new Cimletezo();
+        System.out.println(cimletezo.cimletezoMethod(12345));
+    }
+}
